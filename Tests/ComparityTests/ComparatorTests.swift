33beab4c13b377fb1db54b0a465d39b1594736db
Alexey Karetski <karetski@gmail.com>
2019-06-23 23:26:50 +0300

    Initial tests implementation

--- Tests/ComparityTests/ComparatorTests.swift
+++ Tests/ComparityTests/ComparatorTests.swift
@@ -1,11 +1,26 @@
 import XCTest
-@testable import Comparator
+
+@testable import Comparity
 
 final class ComparatorTests: XCTestCase {
-    func testExample() {
+    private typealias Comparator = Comparity.Comparator
+
+    func testAreInIncreasingOrder() {
+        let comparator = Comparator<Int> { leftParameter, rightParameter in
+            guard leftParameter != rightParameter else {
+                throw Comparator<Int>.Error.same
+            }
+
+            return leftParameter < rightParameter
+        }
+
+        // TODO: Rewrite in more clean way with Testament.
+        XCTAssertTrue((try? comparator.areInIncreasingOrder(0, 1)) ?? false)
+        XCTAssertFalse((try? comparator.areInIncreasingOrder(1, 0)) ?? false)
+        XCTAssertThrowsError(try comparator.areInIncreasingOrder(0, 0))
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testAreInIncreasingOrder", testAreInIncreasingOrder),
     ]
 }
